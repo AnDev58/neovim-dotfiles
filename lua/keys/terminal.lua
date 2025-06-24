d3ed0bc9407a0830fb46852f5e56aa3a307bc333
AnDev58 <andev58@outlook.com>
2025-06-24 13:12:07 +0300

    Added terminal support

---
+++ lua/keys/terminal.lua
@@ -0,0 +1,4 @@
+vim.api.nvim_set_keymap("n", "\\t", ":split term://zsh<CR>", { desc = "Open terminal" })
+vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", {})
+
+vim.cmd([[autocmd TermOpen * startinsert]])
